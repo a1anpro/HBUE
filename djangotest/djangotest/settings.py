d9d805ac8065549b97ef08c903019689ad403086
eternal124 <1241503720@qq.com>
2017-11-02 18:20:45 +0800

    url tst

--- djangotest/djangotest/settings.py
+++ djangotest/djangotest/settings.py
@@ -83,6 +83,7 @@ DATABASES = {
         'NAME': 'hbue',
         'USER': 'root',
         'PASSWORD': '123456',
+        # 'HOST': '120.78.152.114',
         'HOST': 'localhost',
         'PORT': '3306',
     }
@@ -125,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/hbue/static/'
 STATIC_ROOT=[
-    os.path.join(BASE_DIR,'hbue/static')
+    os.path.join(BASE_DIR,'/hbue/static')
 ]
